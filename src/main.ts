b912a7bdbad0e065de4baf8d28a870c2287f24b1
aomsin <thitikorn.k@codium.co>
2020-08-26 20:44:29 +0700

    clean code use class and functional

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,35 @@ class Game {
         this.player1Points = player1Points;
         this.player2Points = player2Points;
     }
-}
 
-export function checkWinner(playerScore:number, rivalScore:number) {
-    return playerScore - rivalScore >=2 && playerScore >= 4;
+    messageScore(): String{
+        if(this.player1Points == this.player2Points){
+            switch(this.player1Points) {
+                case 0:return 'love - love';break;
+                case 1:return 'fifteen - fifteen';break;
+                case 2:return 'thirty - thirty'; break;
+                default:return 'deuce';break;
+            }
+        }else{
+            if(this.player1Points > this.player2Points){
+                return 'advantage player 1';
+            }else{
+                return 'advantage player 2';
+            }
+        }
+    }
+    checkWinner(playerScore:number, rivalScore:number) :boolean {
+        return playerScore - rivalScore >=2 && playerScore >= 4;
+    }
+    winner() :String {
+        if(this.checkWinner(this.player1Points, this.player2Points)) {
+            return 'Player 1';
+        }else if(this.checkWinner(this.player2Points, this.player1Points)) {
+            return 'Player 2';
+        }else{
+            return 'No player win yet';
+        }
+    }
 }
 
 export function updateGameScore(player1Points:number, player2Points:number) {
@@ -20,32 +45,9 @@ export function updateGameScore(player1Points:number, player2Points:number) {
 }
 
 export function decideGameWinner(game:Game) {
-    if(checkWinner(game.player1Points, game.player2Points)) {
-        return 'Player 1';
-    }else if(checkWinner(game.player2Points, game.player1Points)) {
-        return 'Player 2';
-    }else{
-        return 'No player win yet';
-    }
+    return game.winner();
 }
 
 export function printScore(list:any) {
-    return messageScore(updateGameScore(list[0],list[1]));
-}
-
-export function messageScore(game:Game) {
-    if(game.player1Points == game.player2Points){
-        switch(game.player1Points) {
-            case 0:return 'love - love';break;
-            case 1:return 'fifteen - fifteen';break;
-            case 2:return 'thirty - thirty'; break;
-            default:return 'deuce';break;
-        }
-    }else{
-        if(game.player1Points > game.player2Points){
-            return 'advantage player 1';
-        }else{
-            return 'advantage player 2';
-        }
-    }
+    return updateGameScore(list[0],list[1]).messageScore();
 }
